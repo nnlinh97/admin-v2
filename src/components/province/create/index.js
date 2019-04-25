4373720dd6be7412c5a17fe9a89d302ecb1418d9
nnlinh97 <nnlinh97it@gmail.com>
2019-04-26 01:59:49 +0700

    - change modal province

--- src/components/province/create/index.js
+++ src/components/province/create/index.js
@@ -1,16 +1,11 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
 import './../modal.css';
 import * as actions from './../../../actions/index';
-import { URL } from '../../../constants/url';
-import axios from 'axios';
-import { apiGet, apiPost } from './../../../services/api';
 import { newListSelect } from '../../../helper';
 import Modal from 'react-responsive-modal';
 import Select from 'react-select';
 
-class ListTypesComponent extends Component {
+class CreateProvinceComponent extends Component {
 
     constructor(props) {
         super(props);
@@ -26,8 +21,9 @@ class ListTypesComponent extends Component {
         this.setState({ listCountries });
     }
 
-    handleCreate = () => {
-        const {name, country} = this.state;
+    handleCreate = (event) => {
+        event.preventDefault();
+        const { name, country } = this.state;
         this.props.handleCreate(name, country);
     }
 
@@ -35,9 +31,7 @@ class ListTypesComponent extends Component {
         let target = event.target;
         let name = target.name;
         let value = target.value;
-        this.setState({
-            [name]: value
-        });
+        this.setState({ [name]: value });
     }
 
     handleChangeCountry = (selected) => {
@@ -45,40 +39,52 @@ class ListTypesComponent extends Component {
     }
 
     render() {
-        const { name, listCountries } = this.state;
-        return (<div className="modal-content">
-            <div className="modal-header">
-                <h4 className="modal-title">Thêm Mới</h4>
-            </div>
-            <div className="modal-body">
-                <form className="form-horizontal">
-                    <div className="box-body">
-                        <div className="form-group">
-                            <label htmlFor="inputEmail3" className="col-sm-3 control-label">Tên Tỉnh Thành</label>
-                            <div className="col-sm-9">
-                                <input required onChange={this.handleChange} name="name" value={name} type="text" className="form-control" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputEmail3" className="col-sm-3 control-label">Quốc Gia</label>
-                            <div className="col-sm-9">
-                                {listCountries.length > 0 && <Select
-                                    value={this.state.country}
-                                    onChange={this.handleChangeCountry}
-                                    options={newListSelect(listCountries)}
-                                    maxMenuHeight={200}
-                                    placeholder=""
-                                />}
-                            </div>
+        return <div style={{ marginLeft: '0px', height: '285px' }} className="content-wrapper">
+            <section style={{ marginBottom: "20px" }} className="content-header">
+                <h1> Thêm Mới Tỉnh Thành </h1>
+            </section>
+            <section className="content">
+                <div className="row">
+                    <div className="col-lg-12 col-xs-12 ">
+                        <div className="box box-info">
+                            <form onSubmit={this.handleCreate} className="form-horizontal">
+                                <div className="box-body">
+                                    <div className="form-group">
+                                        <label className="col-sm-3 control-label">Tỉnh Thành</label>
+                                        <div className="col-sm-8">
+                                            <input
+                                                type="text"
+                                                onChange={this.handleChange}
+                                                value={this.state.name}
+                                                name="name"
+                                                className="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="box-body">
+                                    <div className="form-group">
+                                        <label className="col-sm-3 control-label">Quốc Gia</label>
+                                        <div className="col-sm-8">
+                                            <Select
+                                                value={this.state.country}
+                                                onChange={this.handleChangeCountry}
+                                                options={newListSelect(this.state.listCountries)}
+                                                maxMenuHeight={200}
+                                                placeholder=""
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="box-footer col-sm-11">
+                                    <button type="submit" className="btn btn-info pull-right">Lưu Thay Đổi</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div className="modal-footer">
-                <button onClick={this.handleCreate} type="button" className="btn btn-primary">Lưu Thay Đổi</button>
-            </div>
-        </div>);
+                </div>
+            </section>
+        </div>;
     }
 }
 
-export default withRouter(ListTypesComponent);
+export default CreateProvinceComponent;
--- src/components/province/edit/index.js
+++ src/components/province/edit/index.js
@@ -1,22 +1,16 @@
 import React, { Component } from 'react';
-import { withRouter } from 'react-router-dom';
-import { connect } from 'react-redux';
-import './../modal.css';
-import * as actions from './../../../actions/index';
-import { URL } from '../../../constants/url';
-import axios from 'axios';
-import { apiGet, apiPost } from './../../../services/api';
+// import './../modal.css';
 import { newListSelect } from '../../../helper';
 import Modal from 'react-responsive-modal';
 import Select from 'react-select';
 
-class ListTypesComponent extends Component {
+class EditProvinceComponent extends Component {
 
     constructor(props) {
         super(props);
         this.state = {
             name: '',
-            listCountries: [],
+            listCountries: null,
             country: ''
         }
     }
@@ -26,10 +20,9 @@ class ListTypesComponent extends Component {
         this.setState({ name, listCountries, country });
     }
 
-
-    handleEdit = () => {
+    handleEdit = (event) => {
+        event.preventDefault();
         const { name, country } = this.state;
-        console.log(name, country.id);
         this.props.handleEdit(name, country);
     }
 
@@ -37,9 +30,7 @@ class ListTypesComponent extends Component {
         let target = event.target;
         let name = target.name;
         let value = target.value;
-        this.setState({
-            [name]: value
-        });
+        this.setState({ [name]: value });
     }
 
     handleChangeCountry = (selected) => {
@@ -48,39 +39,55 @@ class ListTypesComponent extends Component {
 
     render() {
         const { name, listCountries, country } = this.state;
-        return (<div className="modal-content">
-            <div className="modal-header">
-                <h4 className="modal-title">Chỉnh Sửa</h4>
-            </div>
-            <div className="modal-body">
-                <form className="form-horizontal">
-                    <div className="box-body">
-                        <div className="form-group">
-                            <label htmlFor="inputEmail3" className="col-sm-3 control-label">Tên Tỉnh Thành</label>
-                            <div className="col-sm-9">
-                                <input required onChange={this.handleChange} name="name" value={name} type="text" className="form-control" />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <label htmlFor="inputEmail3" className="col-sm-3 control-label">Quốc Gia</label>
-                            <div className="col-sm-9">
-                                {listCountries.length > 0 && <Select
-                                    defaultValue={{ label: country !== '' ? country.name : '', value: country !== '' ? country.id : '' }}
-                                    onChange={this.handleChangeCountry}
-                                    options={newListSelect(listCountries)}
-                                    maxMenuHeight={200}
-                                    placeholder=""
-                                />}
-                            </div>
+        return <div style={{ marginLeft: '0px', height: '285px' }} className="content-wrapper">
+            <section style={{ marginBottom: "20px" }} className="content-header">
+                <h1> Thêm Mới Tỉnh Thành </h1>
+            </section>
+            <section className="content">
+                <div className="row">
+                    <div className="col-lg-12 col-xs-12 ">
+                        <div className="box box-info">
+                            <form onSubmit={this.handleEdit} className="form-horizontal">
+                                <div className="box-body">
+                                    <div className="form-group">
+                                        <label className="col-sm-3 control-label">Tỉnh Thành</label>
+                                        <div className="col-sm-8">
+                                            <input
+                                                type="text"
+                                                onChange={this.handleChange}
+                                                value={this.state.name}
+                                                name="name"
+                                                className="form-control" />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="box-body">
+                                    <div className="form-group">
+                                        <label className="col-sm-3 control-label">Quốc Gia</label>
+                                        <div className="col-sm-8">
+                                            {this.state.listCountries && <Select
+                                                onChange={this.handleChangeCountry}
+                                                options={newListSelect(this.state.listCountries)}
+                                                defaultValue={{
+                                                    label: country !== '' ? country.name : '',
+                                                    value: country !== '' ? country.id : ''
+                                                }}
+                                                maxMenuHeight={200}
+                                                placeholder=""
+                                            />}
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="box-footer col-sm-11">
+                                    <button type="submit" className="btn btn-info pull-right">Lưu Thay Đổi</button>
+                                </div>
+                            </form>
                         </div>
                     </div>
-                </form>
-            </div>
-            <div className="modal-footer">
-                <button onClick={this.handleEdit} type="button" className="btn btn-primary">Lưu Thay Đổi</button>
-            </div>
-        </div>);
+                </div>
+            </section>
+        </div>;
     }
 }
 
-export default withRouter(ListTypesComponent);
+export default EditProvinceComponent;
