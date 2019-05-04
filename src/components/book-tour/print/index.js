61ee2e66156a261a25934f6ea73e5c5d9ffee004
vuonghuuthien <vuonghuuthientk@gmail.com>
2019-05-04 23:24:50 +0700

    Event Print

--- src/components/book-tour/print/index.js
+++ src/components/book-tour/print/index.js
@@ -7,6 +7,7 @@ import * as actions from './../../../actions/index';
 import { apiGet, apiPost } from '../../../services/api';
 import { formatCurrency } from './../../../helper';
 import moment from 'moment';
+import './index.css';
 
 class ListTypesComponent extends Component {
     constructor(props) {
@@ -40,48 +41,89 @@ class ListTypesComponent extends Component {
                     </h1>
                 </section> */}
                 <section className="content">
+                    <div className="title_tour">Tour: Tour From A To B.</div>
                     <div className="row invoice-info">
-                        <div style={{ textAlign: 'right' }} className="col-sm-5 invoice-col">
-                            <address>
-                                <strong>Tour</strong><br />
-                                <strong>Start Date</strong><br />
-                                <strong>End Date</strong><br />
-                                <strong>Price/People</strong><br />
-                                <strong>Total People</strong><br />
-                                <strong>Total Pay</strong><br />
-                                <strong>Contact Name</strong><br />
-                                <strong>Contact Phone</strong><br />
-                            </address>
+                        <div className="row_title_h2">
+                            <h2>Thông tin chuyến đi</h2>
+                            <button><i class="fa fa-print" aria-hidden="true"></i></button>
                         </div>
-                        <div className="row">
+                        <div className="box-body-main">
+                            <div className="box-body-main-left">
+                                <div className="box-body-left">
+                                    <div className="">Ngày Bắt Đầu</div>
+                                    <div className="">Giá/ Người</div>
+                                    <div className="">Giảm Giá</div>
+                                </div>
+                                <div className="box-body-right">
+                                    <div className="">12/12/2012</div>
+                                    <div className="">10000 VND</div>
+                                    <div className="">10%</div>
+                                </div>
+                            </div>
+                            <div className="box-body-main-right">
+                                <div className="box-body-left">
+                                    <div className="">Ngày Kết Thúc</div>
+                                    <div className="">Tổng Người</div>
+                                    <div className="">Tổng tiền</div>
+                                </div>
+                                <div className="box-body-right">
+                                    <div className="">12/12/2012</div>
+                                    <div className="">10 People</div>
+                                    <div className="">90000 VND</div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row">
+                        <h2>Thông tin hành khách</h2>
                             <div className="col-xs-12 table-responsive">
-                                <table className="table table-striped">
+                                <table className="table table-striped table_info_passengers">
                                     <thead>
                                         <tr>
                                             {/* <th>STT</th> */}
-                                            <th>Contact Name</th>
-                                            <th>Phone</th>
-                                            <th>List Passengers</th>
+                                            <th width="20%">Contact Name</th>
+                                            <th width="20%">Phone</th>
+                                            <th width="45%">List Passengers</th>
                                             <th>Note</th>
                                         </tr>
                                     </thead>
                                     <tbody>
-                                        {bookTour && bookTour.book_tour_history.map((item, index) => {
+                                        {/* {bookTour && bookTour.book_tour_history.map((item, index) => {
                                             if (item.status === "paid") {
                                                 return <tr key={index}>
-                                                    {/* <td>{index + 1}</td> */}
+                                                    <td>{index + 1}</td>
                                                     <td>{item.book_tour_contact_info.fullname}</td>
                                                     <td>{item.book_tour_contact_info.phone}</td>
-                                                    <td>{item.passengers.map((passenger, i) => {
-                                                        return <p key={i}>
-                                                            {passenger.fullname}<br />
-                                                        </p>;
-                                                    })}</td>
+
                                                     <td></td>
                                                 </tr>;
                                             }
                                             return null;
-                                        })}
+                                        })} */}
+                                        <td>Lorem Ipsum Summ</td>
+                                        <td>(+11) 1111 1111</td>
+                                        <td>
+                                            <table class="table table-striped mini_table">
+                                                <tbody>
+                                                    <tr>
+                                                        <td>Nguyan Van A</td>
+                                                        <td>Adult</td>
+                                                        <td>Man</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Nguyan Van B</td>
+                                                        <td>Children</td>
+                                                        <td>Woman</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <td>Nguyan Van C</td>
+                                                        <td>Adult</td>
+                                                        <td>Man</td>
+                                                    </tr>
+                                                </tbody>
+                                            </table>
+                                        </td>
+                                        <td></td>
                                     </tbody>
                                 </table>
                             </div>
@@ -92,7 +134,6 @@ class ListTypesComponent extends Component {
                                 </button>
                             </div>
                         </div> */}
-                    </div>
                 </section>
             </div>
         );
