8917bcc5b59e6257cf9f7089506b20ed6cd97b3e
vuonghuuthien <vuonghuuthientk@gmail.com>
2019-05-08 21:55:54 +0700

    Fix Create

--- src/components/tour/create.js
+++ src/components/tour/create.js
@@ -517,18 +517,34 @@ class ListTypesComponent extends Component {
                                         </div>
                                         <div style={{ height: '300px' }} className="form-group">
                                             <label>Feature Image (*)</label>
-                                            <input onChange={this.handleChangeImage} type="file" id="exampleInputFile" /><br />
-                                            {this.state.previewImage !== '' ?
-                                                <img style={{ width: '100%', height: '250px' }} src={this.state.previewImage} /> :
-                                                <img style={{ width: '100%', height: '250px' }} src="http://denrakaev.com/wp-content/uploads/2015/03/no-image-800x511.png" />
-                                            }
+                                            {/* <input onChange={this.handleChangeImage} type="file" id="exampeInputFile" /><br /> */}
+                                            <div className="inputImage" id="exampeInputFile">
+                                                {this.state.previewImage !== '' ?
+                                                    <img src={this.state.previewImage} /> :
+                                                    <img src="../../../public/dist/img/add_image.png" />
+                                                }
+                                                <a>Choose Image</a>
+                                            </div>
                                         </div><br />
                                         <div className="form-group">
                                             <label>Images</label>
-                                            <button onClick={this.openModalListImages} className="pull-right btn btn-default">
+                                            {/* <button onClick={this.openModalListImages} className="pull-right btn btn-default">
                                                 <i className="fa fa-pencil" />
                                             </button>
-                                            <input onChange={this.handleChangeListImages} type="file" multiple />
+                                            <input onChange={this.handleChangeListImages} type="file" multiple /> */}
+                                            <div className="slideshow">
+                                                <div className="imageOfSlideshow">
+                                                    <img></img>
+                                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                                </div>
+                                                <div className="imageOfSlideshow">
+                                                    <img></img>
+                                                    <i class="fa fa-times" aria-hidden="true"></i>
+                                                </div>
+                                                <div className="addImageOfSlideshow">
+                                                    <i class="fa fa-plus" aria-hidden="true"></i>
+                                                </div>
+                                            </div>
                                         </div>
                                         <div className="form-group">
                                             <label>Policy</label>
