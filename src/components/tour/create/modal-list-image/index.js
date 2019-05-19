76a1e0ff0eccfd54ae256991f273854e74e38ce6
vuonghuuthien <vuonghuuthientk@gmail.com>
2019-05-19 23:43:38 +0700

    List Image

--- src/components/tour/create/modal-list-image/index.js
+++ src/components/tour/create/modal-list-image/index.js
@@ -9,11 +9,41 @@ import './index.css';
 class CreateRouteComponent extends Component {
 
     render() {
-        return <div style={{ marginLeft: '0px' }} className="content-wrapper">
+        return <div style={{ marginLeft: '0px' }} className="content-wrapper modal_list_image_pop_up">
             <section style={{ marginBottom: "20px" }} className="content-header">
                 <h1> Danh Sách Hình Ảnh </h1>
             </section>
             <section className="content">
+                <div className="listImage">
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                    <div className="imageOfListImage">
+                        <img></img>
+                        <i class="fa fa-times" aria-hidden="true"></i>
+                    </div>
+                </div>
             </section>
         </div>;
     }
